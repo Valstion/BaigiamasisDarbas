300b53978165e8b7e8c6748170516780c2f2ce0e
Valstion <lordvalstion@gmail.com>
2022-10-13 16:08:24 +0300

    Commit

---
+++ src/test/java/utils/Driver.java
@@ -0,0 +1,32 @@
+package utils;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.time.Duration;
+
+public class Driver {
+
+    private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    public static void setDriver() {
+        WebDriverManager.chromedriver().setup();
+        driver.set(new ChromeDriver(setUpOptions()));
+        driver.get().manage().deleteAllCookies();
+        driver.get().manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    public static WebDriver getDriver() {
+        return driver.get();
+    }
+    public static void close() {
+        driver.get().quit();
+    }
+    private static ChromeOptions setUpOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("window-size=3840,2160");
+        options.addArguments("--force-device-scale-factor=0.75");;
+        return options;
+    }
+}
